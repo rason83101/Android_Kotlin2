90696e56c4b5b26702bb72f40b1713543333b272
rason83101 <a0970729591@gmail.com>
2019-10-23 23:31:10 +0800

    kotlin

---
+++ app/src/main/java/com/example/a106360110_/Main2Activity.kt
@@ -0,0 +1,59 @@
+package com.example.a106360110_
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.R
+
+
+class Main2Activity : AppCompatActivity() {
+    private var send_btn: Button? = null
+    private var set_drink: EditText? = null
+    private var rg1: RadioGroup? = null
+    private var rg2: RadioGroup? = null
+
+    private var sugar = "無糖"
+    private var ice_opt = "微冰"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.a106360110_.R.layout.activity_main2)
+
+        rg1 = findViewById(com.example.a106360110_.R.id.radioGroup)
+        rg1!!.setOnCheckedChangeListener { radioGroup, i ->
+            when (i) {
+                com.example.a106360110_.R.id.radioButton1 -> sugar = "無糖"
+                com.example.a106360110_.R.id.radioButton2 -> sugar = "少糖"
+                com.example.a106360110_.R.id.radioButton3 -> sugar = "半糖"
+                com.example.a106360110_.R.id.radioButton4 -> sugar = "全糖"
+            }
+        }
+        rg2 = findViewById(com.example.a106360110_.R.id.radioGroup2)
+        rg2!!.setOnCheckedChangeListener { radioGroup, i ->
+            when (i) {
+                com.example.a106360110_.R.id.radioButton5 -> ice_opt = "微冰"
+                com.example.a106360110_.R.id.radioButton6 -> ice_opt = "少冰"
+                com.example.a106360110_.R.id.radioButton7 -> ice_opt = "正常冰"
+            }
+        }
+
+        send_btn = findViewById(com.example.a106360110_.R.id.btn_send)
+        send_btn!!.setOnClickListener {
+                set_drink = findViewById(com.example.a106360110_.R.id.ed_drink)
+                val drink = set_drink!!.text.toString()
+                val i = Intent()
+                val b = Bundle()
+                b.putString("sugar", sugar)
+                b.putString("drink", drink)
+                b.putString("ice", ice_opt)
+                i.putExtras(b)
+                setResult(101, i)
+                finish()
+            }
+        }
+    }
